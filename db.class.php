86d97e6f7fc68291184fc1c28f79665ce865a5d9
Sam Crowley <anywaydarkness@gmail.com>
2025-08-15 14:18:45 +0000

    add basic sql

--- db.class.php
+++ db.class.php
@@ -1,5 +1,30 @@
 <?php
 
-	
+	class Db
+	{
+		private $dbh;
+		
+		function __construct($host, $user, $pass, $name)
+		{
+			try
+			{
+				$this->dbh = new PDO('mysql:host='.$host.';dbname='.$name, $user, $pass);
+			}
+			catch(Exception $ex)
+			{
+				die('Error connect to database.');
+				//die('Error. '.$ex->getMessage());
+			}
+		}
+		
+		function query($sql, $queryParams = array())
+		{
+			$sth = $this->dbh->prepare($sql);
+			
+			$sth->execute($queryParams);
+			
+			return $sth->fetchAll();
+		}
+	}
 
 ?>
--- hotel.php
+++ hotel.php
@@ -2,7 +2,14 @@
 
 	// controller hotel.php
 
-	include_once('config.php');
+	include_once('config.php'); // data for connecting to the database, etc
+	include_once('db.class.php');
 	include_once('tpl/hotel.tpl');
+	
+	$db = new Db($dbHost, $dbUser, $dbPass, $dbName);
+	
+	$rooms = $db->query('SELECT * FROM rooms');
+	
+	echo '<pre>'; print_r($rooms);
 
 ?>
